45e9706635b3e1b051511fd555f082dedb6ad49e
Tatyana Samostroenko <tanyasamastr@gmail.com>
2021-08-31 00:41:55 +0300

    Add files via upload

---
+++ Chat/Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  Chat
+//
+//  Created by TanyaSamastr on 23.08.21.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <JSQMessagesViewController/JSQMessages.h>
+
+#endif /* Bridging_Header_h */
